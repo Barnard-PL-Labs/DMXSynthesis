ce8953c3f3cf5c5325915850eb9949d606a21a43
sophie w <sophiew24@lakesideschool.org>
2023-07-24 23:09:17 -0700

    combined librosa with mido to write notes to a midi file

--- get_amplitude.py
+++ get_amplitude.py
@@ -1,46 +1,84 @@
+import mido
+from mido import MidiFile, MidiTrack, Message
 import librosa as librosa
 import numpy as np
-import matplotlib.pyplot as plt
 import librosa.display
-from IPython.display import Audio
-
-## TODO: find a library that can write a DMX file
-
-dmx_data = []
 
 audioFile = 'StarWars60.wav'
 y, sr = librosa.load(audioFile)
 # new_y = librosa.resample(y, orig_sr=sr, target_sr=1000) #lowering the sample rate if necessary
 
-red = 0
-green = 1
+midi_file = MidiFile()
+track = MidiTrack()
+midi_file.tracks.append(track)
 
-i=0
-## finds amplitude values
-while i < len(y):
-    amplitude = abs(y[i])
-    if amplitude < 0.5:
-        dmx_data.append(red)
-        # dmx_data.append(str(amplitude))
-        i+=1
+# sends n times less commands to the light       
+n=100
+
+for s in range(0,len(y),int(sr/n)):
+    temp = (np.abs(y[s:s+int(sr/n)]).mean())
+    if temp < 0.2:
+        track.append(Message('note_on', note=60, velocity=64, time=0))    # Note On (C4)
+        track.append(Message('note_off', note=60, velocity=64, time=500))  # Note Off (C4) after 500ms
+        
     else:
-        dmx_data.append(green)
-        # dmx_data.append(str(amplitude))
-        i+=1
+        track.append(Message('note_on', note=62, velocity=64, time=0))    # Note On (D4)
+        track.append(Message('note_off', note=62, velocity=64, time=500))  # Note Off (D4) after 500ms
 
-# print(dmx_data)
+midi_file.save('output.mid')
 
-## if you want to send n times less commands to the light       
-# second = []
-# n=100
-# for s in range(0,len(y),int(sr/n)):
-#     second.append(np.abs(y[s:s+int(sr/n)]).mean())
-# print(second)
 
-## shows amplitude envelope
-# plt.figure(figsize=(12, 4))
-# librosa.display.waveshow(y, sr=sr)
-# plt.title('Amplitude Envelope')
-# plt.xlabel('Time (s)')
-# plt.ylabel('Amplitude')
-# plt.show()
+## sends midi notes for each signal in the audio file
+#i=0
+# print(len(y))
+# while i < len(y):
+#     amplitude = abs(y[i])
+#     if amplitude < 0.5:
+#         track.append(Message('note_on', note=60, velocity=64, time=0))    # Note On (C4)
+#         track.append(Message('note_off', note=60, velocity=64, time=500))  # Note Off (C4) after 500ms
+#         i+=1
+#     else:
+#         track.append(Message('note_on', note=62, velocity=64, time=0))    # Note On (D4)
+#         track.append(Message('note_off', note=62, velocity=64, time=500))  # Note Off (D4) after 500ms
+#         i+=1
+
+
+## below is for future use; configures channels (can use for different lights)
+# def create_dmx_midi_file(filename):
+#     mid = MidiFile()
+#     track = MidiTrack()
+#     mid.tracks.append(track)
+
+#     # Set DMX channel 1 to value 100
+#     dmx_channel_1 = 1
+#     dmx_value_1 = 100
+#     cc_message_1 = Message('control_change', control=dmx_channel_1, value=dmx_value_1)
+#     track.append(cc_message_1)
+
+#     # Set DMX channel 2 to value 50
+#     dmx_channel_2 = 2
+#     dmx_value_2 = 50
+#     cc_message_2 = Message('control_change', control=dmx_channel_2, value=dmx_value_2)
+#     track.append(cc_message_2)
+
+#     # Add a delay (optional)
+#     track.append(Message('control_change', control=0, value=0, time=100))
+
+#     # Example of triggering DMX channel 3 at full intensity
+#     dmx_channel_3 = 3
+#     dmx_value_full = 127
+#     note_on_message = Message('note_on', note=dmx_channel_3, velocity=dmx_value_full)
+#     track.append(note_on_message)
+
+#     # Add a delay (optional)
+#     track.append(Message('note_on', note=0, velocity=0, time=100))
+
+#     # Example of turning off DMX channel 3
+#     note_off_message = Message('note_off', note=dmx_channel_3, velocity=0)
+#     track.append(note_off_message)
+
+#     # Save the MIDI file
+#     mid.save(filename)
+
+#     midi_file_name = "dmx_control.mid"
+#     create_dmx_midi_file(midi_file_name)
